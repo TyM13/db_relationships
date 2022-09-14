c8b961a0d8a3d7c9dd132606f1366ce1c502cf19
Your Name <you@example.com>
2022-09-13 19:09:32 -0600

    added queries

---
+++ queries.sql
@@ -0,0 +1,6 @@
+insert into citizen_table (name, address, phone_number, date_of_birth)
+values('name_one', '1 fake street', '403-555-5555', '1997-01-20'),
+('name_two', '2 fake street', '403-555-4444', '1997-02-20');
+
+insert into tax_account (date_created, account_number)
+values('2010-10-10', 12), ('2017-08-10', 22)
